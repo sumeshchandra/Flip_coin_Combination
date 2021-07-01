174760b3dbae69dbc6b5d217d394d2e995240c7c
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-07-01 13:00:21 +0530

    singlet combination dict added

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,12 +1,32 @@
-#!/bin/bash -x
+#! /bin/bash -x
 
 echo "Flip Coin Combination"
 
-flipCoin=$((RANDOM%2))
+echo "number of times user want to flip the coin"
+read flip
+echo $flip
 
-if [ $flipCoin -eq 1 ]
+countHead=0
+countTail=0
+declare -A noOfCounts
+for((i=0;i<flip;i++))
+do
+if [ $((RANDOM%2)) -eq 1 ]
 then
-	echo "TAIL"
+((countHead++))
 else
-	echo"HAID"
+((countTail++))
 fi
+done
+echo "head="$countHead
+echo "tail="$countTail
+
+noOfCounts[tail]=$countTail
+noOfCounts[head]=$countHead
+
+echo ${noOfCounts[@]}
+
+PercentageOfTails=$((100*$countTail/$flip))
+echo "Percentage of tails= "$PercentageOfTails
+PercentageOfHeads=$((100*$countHead/$flip))
+echo "Percentage of heads= "$PercentageOfHeads
